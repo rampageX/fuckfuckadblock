788b6ceb93a345a22cfd5e806b36e20ff50270d2
bogachenko <bogachenkove@gmail.com>
2024-06-21 18:58:49 +0300

    Update patches.py
    Correction in the code:
    * Sorting parameter flags.
    * Sorting all domains separated by ### and ##.
    * Code optimization.

--- .github/SCRIPTS/patches.py
+++ .github/SCRIPTS/patches.py
@@ -15,50 +15,41 @@
 
 import os
 def sort_domains_in_file(filename):
+    def sort_domains(domain_part):
+        domains = domain_part.split('|')
+        sorted_domains = sorted(set(domains))
+        return '|'.join(sorted_domains)
     script_dir = os.path.dirname(__file__)
     file_path = os.path.abspath(os.path.join(script_dir, '..', '..', filename))
-    updated_lines = []
-    js_lines = []
-    script_has_text_lines = []
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    for line in lines:
-        if 'domain=' in line:
-            start_index = line.find('domain=') + len('domain=')
-            end_index = line.find(',', start_index)
-            if end_index == -1:
-                end_index = line.find('$', start_index)
-            if end_index == -1:
-                end_index = len(line)
-            domain_part = line[start_index:end_index].strip()
-            domains = set(domain_part.split('|'))
-            sorted_domains = sorted(domains)
-            updated_domain_line = line[:start_index]
-            updated_domain_line += '|'.join(sorted_domains)
-            updated_domain_line += line[end_index:]
-            if not updated_domain_line.endswith('\n'):
-                updated_domain_line += '\n'
-            updated_lines.append(updated_domain_line)
-        elif '##+js(' in line:
-            js_start_index = line.find('##+js(')
-            js_domains_part = line[:js_start_index].strip()
-            js_domains = set(js_domains_part.split(','))
-            sorted_js_domains = sorted(js_domains)
-            updated_js_line = ','.join(sorted_js_domains)
-            updated_js_line += line[js_start_index:].strip() + '\n'
-            updated_lines.append(updated_js_line)
-        elif '##^script:has-text(' in line:
-            script_start_index = line.find('##^script:has-text(')
-            script_domains_part = line[:script_start_index].strip()
-            script_domains = set(script_domains_part.split(','))
-            sorted_script_domains = sorted(script_domains)
-            updated_script_line = ','.join(sorted_script_domains)
-            updated_script_line += line[script_start_index:].strip() + '\n'
-            updated_lines.append(updated_script_line)
-        else:
-            updated_lines.append(line)
-    with open(file_path, 'w') as file:
-        file.writelines(updated_lines)
+    temp_file = file_path + '.tmp'
+    with open(file_path, 'r') as file, open(temp_file, 'w') as temp:
+        for line in file:
+            if 'domain=' in line:
+                start_index = line.find('domain=') + len('domain=')
+                end_index = line.find(',', start_index)
+                if end_index == -1:
+                    end_index = line.find('$', start_index)
+                if end_index == -1:
+                    end_index = len(line)
+                domain_part = line[start_index:end_index].strip()
+                updated_domain_line = line[:start_index] + sort_domains(domain_part) + line[end_index:]
+                if not updated_domain_line.endswith('\n'):
+                    updated_domain_line += '\n'
+                temp.write(updated_domain_line)
+            elif '##' in line or '###' in line:
+                separator_index = line.find('##') if '##' in line else line.find('###')
+                domains_part = line[:separator_index].strip()
+                sorted_domains_line = ','.join(sorted(set(domains_part.split(','))))
+                updated_line = sorted_domains_line + line[separator_index:].strip() + '\n'
+                temp.write(updated_line)
+            elif '$' in line:
+                dollar_index = line.find('$')
+                params_part = line[dollar_index + 1:].strip()
+                sorted_params_line = line[:dollar_index + 1] + ','.join(sorted(set(params_part.split(',')))) + '\n'
+                temp.write(sorted_params_line)
+            else:
+                temp.write(line)
+    os.replace(temp_file, file_path)
     print(f'Patches have been added to file {filename} and overwritten.')
 filename = 'fuckfuckadblock.txt'
 sort_domains_in_file(filename)
